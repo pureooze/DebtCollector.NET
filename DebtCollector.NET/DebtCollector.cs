6273517d46bbc5ce3844cfd710d3aa5769648f76
Uzair Shamim <uzashamim@gmail.com>
2024-04-05 01:21:24 -0400

    Initial commit

---
+++ DebtCollector.NET/DebtCollector.cs
@@ -0,0 +1,40 @@
+﻿namespace DebtCollector.NET;
+
+public abstract class DebtCollector {
+
+    public static void GenerateDebtReports( 
+        string repoPath
+    ) {
+        IList<KeyValuePair<string,int>> mostCommittedResult = HotspotFinder.GetMostCommittedFiles(
+            repoPath
+        ).ToList();
+
+        string mostCommittedFilesFileName = @"mostCommittedFiles.csv";
+        using StreamWriter mostCommittedFilesWriter = new(mostCommittedFilesFileName);
+
+        mostCommittedFilesWriter.WriteLine( $"Key, Value" );
+        foreach (KeyValuePair<string,int> fileCommitCountEntry in mostCommittedResult) {
+            mostCommittedFilesWriter.WriteLine( $"{fileCommitCountEntry.Key}, {fileCommitCountEntry.Value}" );
+        }
+
+        string mostCommittedFile = mostCommittedResult.First().Key;
+        bool xrayResult = HotspotXray.TryGetXray(
+            repoPath, 
+            mostCommittedFile,
+            out Dictionary<string, long> methodChanges
+        );
+
+        if( !xrayResult ) {
+            Console.Error.WriteLine( "Xray failed" );
+            return;
+        }
+
+        IEnumerable<KeyValuePair<string, long>> methodChangesFromMostToLeast = methodChanges.OrderBy( m => m.Value ).Reverse();
+        string xrayFileName = @"xray.csv";
+        using StreamWriter xrayWriter = new(xrayFileName);
+        xrayWriter.WriteLine( $"Key, Value" );
+        foreach (KeyValuePair<string, long> methodChange in methodChangesFromMostToLeast) {
+            xrayWriter.WriteLine( $"{methodChange.Key}, {methodChange.Value}" );
+        }
+    }
+}
---
+++ DebtCollector.NET/HotspotFinder.cs
@@ -0,0 +1,40 @@
+﻿using LibGit2Sharp;
+
+namespace DebtCollector.NET;
+
+public abstract class HotspotFinder {
+
+    public static IOrderedEnumerable<KeyValuePair<string,int>> GetMostCommittedFiles( 
+        string pathToRepo 
+    ) {
+
+        DateTimeOffset since = DateTimeOffset.Now.AddYears(-1);
+
+        using Repository repo = new(pathToRepo);
+        CommitFilter filter = new() {
+            IncludeReachableFrom = repo.Branches
+        };
+
+        ICommitLog? commitLog = repo.Commits.QueryBy( filter );
+        IEnumerable<Commit> commits = commitLog.Where( c => c.Committer.When > since );
+
+        Dictionary<string, int> fileCommitCounts = new();
+            
+        foreach (Commit commit in commits) {
+            foreach (Commit? parent in commit.Parents) {
+                TreeChanges? changesSincePreviousCommit = repo.Diff.Compare<TreeChanges>( 
+                    oldTree: parent.Tree, 
+                    newTree: commit.Tree 
+                );
+                    
+                foreach (TreeEntryChanges? change in changesSincePreviousCommit) {
+                    if (!fileCommitCounts.TryAdd( change.Path, 1 )) {
+                        fileCommitCounts[change.Path]++;
+                    }
+                }
+            }
+        }
+        
+        return fileCommitCounts.OrderByDescending( x => x.Value );
+    }
+}
